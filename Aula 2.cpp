4ae7624b3ed844cde1c12ea0eb4f3da942d1a3a9
pedromarinhomachado <73441554+pedromarinhomachado@users.noreply.github.com>
2024-04-16 14:20:40 -0300

    Add files via upload

---
+++ Aula 2.cpp
@@ -0,0 +1,9 @@
+//Aula 2
+//Pedro Marinho Machado - 16/04/2024
+#include <stdio.h>
+int main(){
+	float raio, area, pi=3.14;
+	printf("Digite o raio do circulo que deseja saber a area: \n");
+	scanf("%f", &raio);
+	area= pi*(raio*raio);
+	printf("A area deste circulo e de: %f", area);
